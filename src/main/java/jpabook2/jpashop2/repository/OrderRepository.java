41ce736a5fe43fbd52f9715a02eca7b454420abb
WooHCode <gusdn526@naver.com>
2022-11-23 02:57:58 +0900

    Dynamic Query

--- src/main/java/jpabook2/jpashop2/repository/OrderRepository.java
+++ src/main/java/jpabook2/jpashop2/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package jpabook2.jpashop2.repository;
 
 import jpabook2.jpashop2.domain.Order;
+import jpabook2.jpashop2.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -18,5 +23,67 @@ public class OrderRepository {
     public Order findOne(Long orderId){
         return em.find(Order.class, orderId);
     }
-    // public List<Order> findAll(OrderSerch orderSerch){}
+
+    public List<Order> findAllByString(OrderSearch orderSearch){
+        //language=JPAQL
+      String jpql = "select o From Order o join o.member m";
+      boolean isFirstCondition = true;
+
+      //주문 상태 검색
+      if (orderSearch.getOrderStatus() != null) {
+          if (isFirstCondition) {
+              jpql += " where";
+              isFirstCondition = false;
+          }else {
+              jpql += " and";
+          }
+          jpql += " o.status = :status";
+      }
+      //회원이름검색
+      if (StringUtils.hasText(orderSearch.getMemberName())){
+          if (isFirstCondition) {
+              jpql += " where";
+              isFirstCondition = false;
+          } else {
+              jpql += " and";
+          }
+          jpql += " m.name like :name";
+      }
+      TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000);
+
+      if (orderSearch.getOrderStatus() != null){
+          query = query.setParameter("status", orderSearch.getOrderStatus());
+      }
+      if (StringUtils.hasText(orderSearch.getMemberName())){
+          query = query.setParameter("name", orderSearch.getMemberName());
+      }
+
+      return query.getResultList();
+  }
+
+    /**
+     * JPA Criteria
+     */
+  public List<Order> findAllByCriteria(OrderSearch orderSearch){
+      CriteriaBuilder cb = em.getCriteriaBuilder();
+      CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+      Root<Order> o = cq.from(Order.class);
+      Join<Object, Object> m = o.join("member", JoinType.INNER); // 회원과 조인
+
+      List<Predicate> criteria = new ArrayList<>();
+
+      //주문 상태 검색
+      if (orderSearch.getOrderStatus() != null){
+          Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+          criteria.add(status);
+      }
+      //회원 이름 검색
+      if (StringUtils.hasText(orderSearch.getMemberName())){
+          Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+          criteria.add(name);
+      }
+      cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+      TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+      return query.getResultList();
+  }
 }
