a277a705ff65c9bed8e3b402d8406bd32a64a054
WooHCode <gusdn526@naver.com>
2022-11-23 19:08:37 +0900

    controller(view)

---
+++ src/main/java/jpabook2/jpashop2/controller/MemberController.java
@@ -0,0 +1,36 @@
+package jpabook2.jpashop2.controller;
+
+import jpabook2.jpashop2.domain.Address;
+import jpabook2.jpashop2.domain.Member;
+import jpabook2.jpashop2.service.MemberService;
+import jpabook2.jpashop2.web.MemberForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    public String create(MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
